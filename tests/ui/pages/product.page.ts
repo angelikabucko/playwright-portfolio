02ce9c94e48e40530e64472d9bbea0cda7d53a1a
Angel Bucko <angelbunia9@gmail.com>
2025-06-23 20:13:28 +0100

    Refactor: Changed locators to be private, added functions - product page

--- tests/ui/pages/product.page.ts
+++ tests/ui/pages/product.page.ts
@@ -1,15 +1,32 @@
-import { type Locator, type Page } from '@playwright/test';
+import { type Locator, type Page, expect } from '@playwright/test';
 
 export class ProductPage {
-  readonly page: Page;
-  readonly productDetails: Locator;
-  readonly addToCartButton: Locator;
-  readonly newItemAdded: Locator;
+  private readonly page: Page;
+  private readonly productDetails: Locator;
+  private readonly addToCartButton: Locator;
+  private readonly newItemAdded: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.productDetails = page.getByText('Sauce Labs Backpack');
     this.addToCartButton = page.getByRole('button', { name: 'ADD TO CART' });
     this.newItemAdded = page.getByRole('link', { name: '1' });
+  }
+
+  async productDetailsIsVisible() {
+    try {
+      await expect(this.productDetails).toBeVisible();
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  async clickAddToCartButton() {
+    await this.addToCartButton.click();
+  }
+
+  async clickNewItemAddedLink() {
+    await this.newItemAdded.click();
   }
 }
