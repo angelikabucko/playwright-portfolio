3c301305c1e9285efb0729fe67b7cc2d2a3713f0
Angel Bucko <angelbunia9@gmail.com>
2025-06-23 20:14:12 +0100

    Refactor: Changed locators to be private, added functions - confirmation page

--- tests/ui/pages/confirmation.page.ts
+++ tests/ui/pages/confirmation.page.ts
@@ -1,11 +1,11 @@
-import { type Locator, type Page } from '@playwright/test';
+import { type Locator, type Page, expect } from '@playwright/test';
 
 export class ConfirmationPage {
-  readonly page: Page;
-  readonly finishHeader: Locator;
-  readonly completeHeader: Locator;
-  readonly menuButton: Locator;
-  readonly logOutLink: Locator;
+  private readonly page: Page;
+  private readonly finishHeader: Locator;
+  private readonly completeHeader: Locator;
+  private readonly menuButton: Locator;
+  private readonly logOutLink: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -13,5 +13,31 @@ export class ConfirmationPage {
     this.completeHeader = page.getByText('THANK YOU FOR YOUR ORDER');
     this.menuButton = page.getByRole('button', { name: 'Open menu' });
     this.logOutLink = page.getByRole('link', { name: 'Logout' });
+  }
+
+  async clickMenuButton() {
+    await this.menuButton.click();
+  }
+
+  async clickLogOutLink() {
+    await this.logOutLink.click();
+  }
+
+  async finishHeaderIsVisible() {
+    try {
+      await expect(this.finishHeader).toBeVisible();
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  async completeHeaderIsVisible() {
+    try {
+      await expect(this.completeHeader).toBeVisible();
+      return true;
+    } catch {
+      return false;
+    }
   }
 }
